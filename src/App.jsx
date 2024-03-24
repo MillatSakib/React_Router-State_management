2a3dd7706b6ae7acc0678fee6315e114ba80cc29
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-03-24 08:04:35 +0600

    bar chart completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Nav from "./Navbar/Nav";
 import React from "react";
 import PriceOptions from "./Price/PriceOptions";
+import Phones from "./Phones/Phones";
 function App() {
   return (
     <>
       <div>
         <Nav></Nav>
         <PriceOptions></PriceOptions>
+        <Phones></Phones>
       </div>
     </>
   );
---
+++ src/Phones/Phones.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const Phones = () => {
+  const [phones, setPhones] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const phoneData = data.data.data;
+        const phonesWithFakeData = phoneData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
+        });
+        setPhones(phonesWithFakeData);
+      });
+  }, []);
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Phones{phones.length}</h2>
+      <BarChart width={1500} height={700} data={phones}>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
+      </BarChart>
+    </div>
+  );
+};
+
+export default Phones;
